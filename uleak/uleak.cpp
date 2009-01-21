da74eb22d97c27693fff96f4ad55a691fbd8e654
dron@infosec.ru <dron@infosec.ru@6bcbea68-ce0e-11dd-8ca8-e3ff79f713b6>
2009-01-21 20:24:16 +0000

    r883@mdf4:  dron | 2009-01-21 23:24:06 +0300  Заработал под linux, только вот коллстек работает не всегда. Его надо заглушить большим значением. в firefox и без того обнаруживаются проблемы.
    git-svn-id: http://uleak.googlecode.com/svn@8 6bcbea68-ce0e-11dd-8ca8-e3ff79f713b6

--- uleak/uleak.cpp
+++ uleak/uleak.cpp
@@ -46,7 +46,7 @@ const uint32_t call_points = 4096;
 const uint32_t block_limit = 1000;
 
 // Отображать стек вызова. В случае лика.
-const bool show_leak_callstack = true;
+const bool show_leak_callstack = false;
 
 // Типы операторов освобождения должны соответствовать операторам выделения.
 enum {
@@ -92,19 +92,6 @@ struct callpoint scps[call_points];
 // -----------------------------------------------------------------------------
 // Утилиты всякие
 
-extern "C" void *_lock_init(void *, int, void *, void *);
-extern "C" void *_kse_alloc(void *, int);
-extern "C" void *_kcb_ctor(void *);
-extern "C" int _lockuser_init(void *, void *);
-extern "C" void *_kseg_alloc(void *);
-extern "C" int _pq_alloc(void *, int, int);
-extern "C" void *_thr_alloc(void *);
-extern "C" void *_rtld_allocate_tls(void *, size_t, size_t);
-extern "C" void _libpthread_init(void *);
-
-extern "C" void _lock_destroy(void *);
-extern "C" void _rtld_free_tls(void *, size_t, size_t);
-
 bool isFunction (void *func, uint32_t cp, size_t fsz)
 {
 	if ((uint32_t)func < cp && cp - (uint32_t)func < fsz)
@@ -148,92 +135,92 @@ void *getReturnAddress (int level)
 }
 
 // Это такие хитрые ссылки на функции...
-extern "C" void *_ZNSs4_Rep9_S_createEjjRKSaIcE;
-extern "C" void *_ZNSbIwSt11char_traitsIwESaIwEE4_Rep9_S_createEjjRKS1_;
-extern "C" void *_ZN9__gnu_cxx13new_allocatorI6OpenedE8allocateEjPKv;
-extern "C" void *_ZN9__gnu_cxx13new_allocatorIN10__gnu_norm10_List_nodeISt4pairISs14TDateTimeEntryEEEE8allocateEjPKv;
-extern "C" void *_ZN9__gnu_cxx13new_allocatorIN14TDateTimeEntry12TimeIntervalEE8allocateEjPKv;
-extern "C" void *_ZN9__gnu_cxx13new_allocatorIN10__gnu_norm10_List_nodeIN5boost13intrusive_ptrINS3_10statechart6detail10leaf_stateISaIvENS6_11rtti_policyEEEEEEEE8allocateEjPKv;
-extern "C" void *_ZN9__gnu_cxx13new_allocatorI8PppStateE8allocateEjPKv;
-extern "C" void *_ZNSs12_S_constructIPKcEEPcT_S3_RKSaIcESt20forward_iterator_tag;
-extern "C" void *_ZN13shared_membufC1Ej;
-extern "C" void *_ZN9__gnu_cxx13new_allocatorIN5boost11multi_index6detail18ordered_index_nodeINS4_INS3_15index_node_baseIN13ClientManager11TimeoutInfoEEEEEEEE8allocateEjPKv;
-extern "C" void *_ZN10__gnu_norm10_List_baseISt4pairISs14TDateTimeEntryESaIS3_EE11_M_get_nodeEv;
-extern "C" void *_ZN10__gnu_norm12_Vector_baseIN14TDateTimeEntry12TimeIntervalESaIS2_EE11_M_allocateEj;
-extern "C" void *_ZNSs7reserveEj;
-extern "C" void *_ZN5boost10statechart6detail8allocateI6OpenedSaIvEEEPvj;
-extern "C" void *_ZNSsC1EPKcRKSaIcE;
-extern "C" void *_ZN10__gnu_norm12_Vector_baseIN14TDateTimeEntry12TimeIntervalESaIS2_EEC2EjRKS3_;
-extern "C" void *_ZN10__gnu_norm4listISt4pairISs14TDateTimeEntryESaIS3_EE14_M_create_nodeERKS3_;
-extern "C" void *_ZN5boost10statechart12simple_stateI6Opened8PppStateNS_3mpl4listIN4mpl_2naES7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_EELNS0_12history_modeE0EEnwEj;
-extern "C" void *_ZN5boost10statechart6detail8allocateI8PppStateSaIvEEEPvj;
-extern "C" void *_ZN9__gnu_cxx13new_allocatorIN10__gnu_norm10_List_nodeIN5boost13intrusive_ptrINS3_10statechart6detail10leaf_stateISaIvENS6_11rtti_policyEEEEEEEE8allocateEjPKv;
-extern "C" void *_ZN9__gnu_cxx13new_allocatorIN10__gnu_norm10_List_nodeI7TSubnetEEE8allocateEjPKv;
-extern "C" void *_ZNSs6appendEPKcj;
-extern "C" void *_ZN5boost6detail12shared_countC1I10iALG_R3411EEPT_;
-extern "C" void *_ZN5boost6detail12shared_countC1IPcNS_21checked_array_deleterIcEEEET_T0_;
-extern "C" void *_ZN10__gnu_norm4listISt4pairISs14TDateTimeEntryESaIS3_EE9_M_insertENS_14_List_iteratorIS3_EERKS3_;
-extern "C" void *_ZN10__gnu_norm6vectorIN14TDateTimeEntry12TimeIntervalESaIS2_EEC2ERKS4_;
-extern "C" void *_ZN10__gnu_norm10_List_baseIN5boost13intrusive_ptrINS1_10statechart6detail10leaf_stateISaIvENS4_11rtti_policyEEEEESaIS9_EE11_M_get_nodeEv;
-extern "C" void *_ZN5boost10statechart12simple_stateI8PppState12StateMachine7InitialLNS0_12history_modeE0EEnwEj;
-extern "C" void *_ZN5boost12shared_arrayIcEC1EPc;
-extern "C" void *_ZN5boost10shared_ptrI10iALG_R3411EC1IS1_EEPT_;
-extern "C" void *_ZN9__gnu_cxx13new_allocatorISt13_Rb_tree_nodeISt4pairIKSsN13ClientManager8PeerInfoEEEE8allocateEjPKv;
-extern "C" void *_ZN10__gnu_norm4listISt4pairISs14TDateTimeEntryESaIS3_EE18_M_insert_dispatchINS_20_List_const_iteratorIS3_EEEEvNS_14_List_iteratorIS3_EET_SB_12__false_type;
-extern "C" void *_ZN14TDateTimeEntryC1ERKS_;
-extern "C" void *_ZN5boost10statechart12simple_stateI8PppState12StateMachine7InitialLNS0_12history_modeE0EE17shallow_constructERKPNS0_13state_machineIS3_S2_SaIvENS0_25null_exception_translatorEEERSA_;
-extern "C" void *_ZN10__gnu_norm4listIN5boost13intrusive_ptrINS1_10statechart6detail10leaf_stateISaIvENS4_11rtti_policyEEEEESaIS9_EE14_M_create_nodeERKS9_;
-extern "C" void *_ZN5boost10statechart12simple_stateI6Opened8PppStateNS_3mpl4listIN4mpl_2naES7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_EELNS0_12history_modeE0EE17shallow_constructERKNS_13intrusive_ptrIS3_EERNS0_13state_machineI12StateMachineS3_SaIvENS0_25null_exception_translatorEEE;
-extern "C" void *_ZN5boost11multi_index21multi_index_containerIN13ClientManager11TimeoutInfoENS0_10indexed_byINS0_18ordered_non_uniqueINS0_6memberIS3_7timevalXadL_ZNS3_2tvEEEEEN4mpl_2naESA_EENS0_14ordered_uniqueINS6_IS3_SsXadL_ZNS3_4hookEEEEESA_SA_EESA_SA_SA_SA_SA_SA_SA_SA_SA_SA_SA_SA_SA_SA_SA_SA_SA_SA_EESaIS3_EE13allocate_nodeEv;
-extern "C" void *_ZN9__gnu_cxx13new_allocatorI7ClosingE8allocateEjPKv;
+// extern "C" void *_ZNSs4_Rep9_S_createEjjRKSaIcE;
+// extern "C" void *_ZNSbIwSt11char_traitsIwESaIwEE4_Rep9_S_createEjjRKS1_;
+// extern "C" void *_ZN9__gnu_cxx13new_allocatorI6OpenedE8allocateEjPKv;
+// extern "C" void *_ZN9__gnu_cxx13new_allocatorIN10__gnu_norm10_List_nodeISt4pairISs14TDateTimeEntryEEEE8allocateEjPKv;
+// extern "C" void *_ZN9__gnu_cxx13new_allocatorIN14TDateTimeEntry12TimeIntervalEE8allocateEjPKv;
+// extern "C" void *_ZN9__gnu_cxx13new_allocatorIN10__gnu_norm10_List_nodeIN5boost13intrusive_ptrINS3_10statechart6detail10leaf_stateISaIvENS6_11rtti_policyEEEEEEEE8allocateEjPKv;
+// extern "C" void *_ZN9__gnu_cxx13new_allocatorI8PppStateE8allocateEjPKv;
+// extern "C" void *_ZNSs12_S_constructIPKcEEPcT_S3_RKSaIcESt20forward_iterator_tag;
+// extern "C" void *_ZN13shared_membufC1Ej;
+// extern "C" void *_ZN9__gnu_cxx13new_allocatorIN5boost11multi_index6detail18ordered_index_nodeINS4_INS3_15index_node_baseIN13ClientManager11TimeoutInfoEEEEEEEE8allocateEjPKv;
+// extern "C" void *_ZN10__gnu_norm10_List_baseISt4pairISs14TDateTimeEntryESaIS3_EE11_M_get_nodeEv;
+// extern "C" void *_ZN10__gnu_norm12_Vector_baseIN14TDateTimeEntry12TimeIntervalESaIS2_EE11_M_allocateEj;
+// extern "C" void *_ZNSs7reserveEj;
+// extern "C" void *_ZN5boost10statechart6detail8allocateI6OpenedSaIvEEEPvj;
+// extern "C" void *_ZNSsC1EPKcRKSaIcE;
+// extern "C" void *_ZN10__gnu_norm12_Vector_baseIN14TDateTimeEntry12TimeIntervalESaIS2_EEC2EjRKS3_;
+// extern "C" void *_ZN10__gnu_norm4listISt4pairISs14TDateTimeEntryESaIS3_EE14_M_create_nodeERKS3_;
+// extern "C" void *_ZN5boost10statechart12simple_stateI6Opened8PppStateNS_3mpl4listIN4mpl_2naES7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_EELNS0_12history_modeE0EEnwEj;
+// extern "C" void *_ZN5boost10statechart6detail8allocateI8PppStateSaIvEEEPvj;
+// extern "C" void *_ZN9__gnu_cxx13new_allocatorIN10__gnu_norm10_List_nodeIN5boost13intrusive_ptrINS3_10statechart6detail10leaf_stateISaIvENS6_11rtti_policyEEEEEEEE8allocateEjPKv;
+// extern "C" void *_ZN9__gnu_cxx13new_allocatorIN10__gnu_norm10_List_nodeI7TSubnetEEE8allocateEjPKv;
+// extern "C" void *_ZNSs6appendEPKcj;
+// extern "C" void *_ZN5boost6detail12shared_countC1I10iALG_R3411EEPT_;
+// extern "C" void *_ZN5boost6detail12shared_countC1IPcNS_21checked_array_deleterIcEEEET_T0_;
+// extern "C" void *_ZN10__gnu_norm4listISt4pairISs14TDateTimeEntryESaIS3_EE9_M_insertENS_14_List_iteratorIS3_EERKS3_;
+// extern "C" void *_ZN10__gnu_norm6vectorIN14TDateTimeEntry12TimeIntervalESaIS2_EEC2ERKS4_;
+// extern "C" void *_ZN10__gnu_norm10_List_baseIN5boost13intrusive_ptrINS1_10statechart6detail10leaf_stateISaIvENS4_11rtti_policyEEEEESaIS9_EE11_M_get_nodeEv;
+// extern "C" void *_ZN5boost10statechart12simple_stateI8PppState12StateMachine7InitialLNS0_12history_modeE0EEnwEj;
+// extern "C" void *_ZN5boost12shared_arrayIcEC1EPc;
+// extern "C" void *_ZN5boost10shared_ptrI10iALG_R3411EC1IS1_EEPT_;
+// extern "C" void *_ZN9__gnu_cxx13new_allocatorISt13_Rb_tree_nodeISt4pairIKSsN13ClientManager8PeerInfoEEEE8allocateEjPKv;
+// extern "C" void *_ZN10__gnu_norm4listISt4pairISs14TDateTimeEntryESaIS3_EE18_M_insert_dispatchINS_20_List_const_iteratorIS3_EEEEvNS_14_List_iteratorIS3_EET_SB_12__false_type;
+// extern "C" void *_ZN14TDateTimeEntryC1ERKS_;
+// extern "C" void *_ZN5boost10statechart12simple_stateI8PppState12StateMachine7InitialLNS0_12history_modeE0EE17shallow_constructERKPNS0_13state_machineIS3_S2_SaIvENS0_25null_exception_translatorEEERSA_;
+// extern "C" void *_ZN10__gnu_norm4listIN5boost13intrusive_ptrINS1_10statechart6detail10leaf_stateISaIvENS4_11rtti_policyEEEEESaIS9_EE14_M_create_nodeERKS9_;
+// extern "C" void *_ZN5boost10statechart12simple_stateI6Opened8PppStateNS_3mpl4listIN4mpl_2naES7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_EELNS0_12history_modeE0EE17shallow_constructERKNS_13intrusive_ptrIS3_EERNS0_13state_machineI12StateMachineS3_SaIvENS0_25null_exception_translatorEEE;
+// extern "C" void *_ZN5boost11multi_index21multi_index_containerIN13ClientManager11TimeoutInfoENS0_10indexed_byINS0_18ordered_non_uniqueINS0_6memberIS3_7timevalXadL_ZNS3_2tvEEEEEN4mpl_2naESA_EENS0_14ordered_uniqueINS6_IS3_SsXadL_ZNS3_4hookEEEEESA_SA_EESA_SA_SA_SA_SA_SA_SA_SA_SA_SA_SA_SA_SA_SA_SA_SA_SA_SA_EESaIS3_EE13allocate_nodeEv;
+// extern "C" void *_ZN9__gnu_cxx13new_allocatorI7ClosingE8allocateEjPKv;
 
 uint32_t getFilteredCallPoint(uint32_t ocp)
 {
-	if (	isFunction(&_ZNSs4_Rep9_S_createEjjRKSaIcE, ocp, 0x100) ||
-		isFunction(&_ZNSbIwSt11char_traitsIwESaIwEE4_Rep9_S_createEjjRKS1_, ocp, 0x100) ||
-		isFunction(&_ZN9__gnu_cxx13new_allocatorI6OpenedE8allocateEjPKv, ocp, 0x20) ||
-		isFunction(&_ZN9__gnu_cxx13new_allocatorIN10__gnu_norm10_List_nodeISt4pairISs14TDateTimeEntryEEEE8allocateEjPKv, ocp, 0x40) ||
-		isFunction(&_ZN9__gnu_cxx13new_allocatorIN14TDateTimeEntry12TimeIntervalEE8allocateEjPKv, ocp, 0x20) ||
-		isFunction(&_ZN9__gnu_cxx13new_allocatorIN10__gnu_norm10_List_nodeIN5boost13intrusive_ptrINS3_10statechart6detail10leaf_stateISaIvENS6_11rtti_policyEEEEEEEE8allocateEjPKv, ocp, 0x10) ||
-		isFunction(&_ZN9__gnu_cxx13new_allocatorIN5boost11multi_index6detail18ordered_index_nodeINS4_INS3_15index_node_baseIN13ClientManager11TimeoutInfoEEEEEEEE8allocateEjPKv, ocp, 0x20) ||
-		isFunction(&_ZN9__gnu_cxx13new_allocatorI8PppStateE8allocateEjPKv, ocp, 0x20) ||
-		isFunction(&_ZNSs12_S_constructIPKcEEPcT_S3_RKSaIcESt20forward_iterator_tag, ocp, 0x80) ||
-		isFunction(&_ZN13shared_membufC1Ej, ocp, 0x40) ||
-		isFunction(&_ZN10__gnu_norm10_List_baseISt4pairISs14TDateTimeEntryESaIS3_EE11_M_get_nodeEv, ocp, 0x40) ||
-		isFunction(&_ZN10__gnu_norm12_Vector_baseIN14TDateTimeEntry12TimeIntervalESaIS2_EE11_M_allocateEj, ocp, 0x40) ||
-		isFunction(&_ZNSs7reserveEj, ocp, 0x100) ||
-		isFunction(&_ZN5boost10statechart6detail8allocateI6OpenedSaIvEEEPvj, ocp, 0x100) ||
-		isFunction(&_ZNSsC1EPKcRKSaIcE, ocp, 0x40) ||
-		isFunction(&_ZN10__gnu_norm12_Vector_baseIN14TDateTimeEntry12TimeIntervalESaIS2_EEC2EjRKS3_, ocp, 0x80) ||
-		isFunction(&_ZN10__gnu_norm4listISt4pairISs14TDateTimeEntryESaIS3_EE14_M_create_nodeERKS3_, ocp, 0x80) ||
-		isFunction(&_ZN5boost10statechart12simple_stateI6Opened8PppStateNS_3mpl4listIN4mpl_2naES7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_EELNS0_12history_modeE0EEnwEj, ocp, 0x20) ||
-		isFunction(&_ZN5boost10statechart6detail8allocateI8PppStateSaIvEEEPvj, ocp, 0x100) ||
-		isFunction(&_ZN9__gnu_cxx13new_allocatorIN10__gnu_norm10_List_nodeIN5boost13intrusive_ptrINS3_10statechart6detail10leaf_stateISaIvENS6_11rtti_policyEEEEEEEE8allocateEjPKv, ocp, 0x20) ||
-		isFunction(&_ZN9__gnu_cxx13new_allocatorIN10__gnu_norm10_List_nodeI7TSubnetEEE8allocateEjPKv, ocp, 0x20) ||
-		isFunction(&_ZNSs6appendEPKcj, ocp, 0x100) ||
-		isFunction(&_ZN5boost6detail12shared_countC1I10iALG_R3411EEPT_, ocp, 0x100) ||
-		isFunction(&_ZN5boost6detail12shared_countC1IPcNS_21checked_array_deleterIcEEEET_T0_, ocp, 0x100) ||
-		isFunction(&_ZN10__gnu_norm4listISt4pairISs14TDateTimeEntryESaIS3_EE9_M_insertENS_14_List_iteratorIS3_EERKS3_, ocp, 0x40) ||
-		isFunction(&_ZN10__gnu_norm6vectorIN14TDateTimeEntry12TimeIntervalESaIS2_EEC2ERKS4_, ocp, 0x100) ||
-		isFunction(&_ZN10__gnu_norm10_List_baseIN5boost13intrusive_ptrINS1_10statechart6detail10leaf_stateISaIvENS4_11rtti_policyEEEEESaIS9_EE11_M_get_nodeEv, ocp, 0x40) ||
-		isFunction(&_ZN5boost10statechart12simple_stateI8PppState12StateMachine7InitialLNS0_12history_modeE0EEnwEj, ocp, 0x20) ||
-		isFunction(&_ZN5boost12shared_arrayIcEC1EPc, ocp, 0x40) ||
-		isFunction(&_ZN5boost10shared_ptrI10iALG_R3411EC1IS1_EEPT_, ocp, 0x80) ||
-		isFunction(&_ZN9__gnu_cxx13new_allocatorISt13_Rb_tree_nodeISt4pairIKSsN13ClientManager8PeerInfoEEEE8allocateEjPKv, ocp, 0x20) ||
-		isFunction(&_ZN10__gnu_norm4listISt4pairISs14TDateTimeEntryESaIS3_EE18_M_insert_dispatchINS_20_List_const_iteratorIS3_EEEEvNS_14_List_iteratorIS3_EET_SB_12__false_type, ocp, 0x80) ||
-		isFunction(&_ZN14TDateTimeEntryC1ERKS_, ocp, 0x40) ||
-		isFunction(&_ZN5boost10statechart12simple_stateI8PppState12StateMachine7InitialLNS0_12history_modeE0EE17shallow_constructERKPNS0_13state_machineIS3_S2_SaIvENS0_25null_exception_translatorEEERSA_, ocp, 0x100) ||
-		isFunction(&_ZN10__gnu_norm4listIN5boost13intrusive_ptrINS1_10statechart6detail10leaf_stateISaIvENS4_11rtti_policyEEEEESaIS9_EE14_M_create_nodeERKS9_, ocp, 0x80) ||
-		isFunction(&_ZN5boost10statechart12simple_stateI6Opened8PppStateNS_3mpl4listIN4mpl_2naES7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_EELNS0_12history_modeE0EE17shallow_constructERKNS_13intrusive_ptrIS3_EERNS0_13state_machineI12StateMachineS3_SaIvENS0_25null_exception_translatorEEE, ocp, 0x100) ||
-		isFunction(&_ZN5boost11multi_index21multi_index_containerIN13ClientManager11TimeoutInfoENS0_10indexed_byINS0_18ordered_non_uniqueINS0_6memberIS3_7timevalXadL_ZNS3_2tvEEEEEN4mpl_2naESA_EENS0_14ordered_uniqueINS6_IS3_SsXadL_ZNS3_4hookEEEEESA_SA_EESA_SA_SA_SA_SA_SA_SA_SA_SA_SA_SA_SA_SA_SA_SA_SA_SA_SA_EESaIS3_EE13allocate_nodeEv, ocp, 0x40) ||
-		isFunction(&_ZN9__gnu_cxx13new_allocatorI7ClosingE8allocateEjPKv, ocp, 0x20) //||
-
-	)
-	{
-		int i = 0;
-		while (reinterpret_cast<uint32_t>(getReturnAddress(i)) != ocp) i++;
-		return getFilteredCallPoint (reinterpret_cast<uint32_t>(getReturnAddress(i + 1)));
-	}
+// 	if (	isFunction(&_ZNSs4_Rep9_S_createEjjRKSaIcE, ocp, 0x100) ||
+// 		isFunction(&_ZNSbIwSt11char_traitsIwESaIwEE4_Rep9_S_createEjjRKS1_, ocp, 0x100) ||
+// 		isFunction(&_ZN9__gnu_cxx13new_allocatorI6OpenedE8allocateEjPKv, ocp, 0x20) ||
+// 		isFunction(&_ZN9__gnu_cxx13new_allocatorIN10__gnu_norm10_List_nodeISt4pairISs14TDateTimeEntryEEEE8allocateEjPKv, ocp, 0x40) ||
+// 		isFunction(&_ZN9__gnu_cxx13new_allocatorIN14TDateTimeEntry12TimeIntervalEE8allocateEjPKv, ocp, 0x20) ||
+// 		isFunction(&_ZN9__gnu_cxx13new_allocatorIN10__gnu_norm10_List_nodeIN5boost13intrusive_ptrINS3_10statechart6detail10leaf_stateISaIvENS6_11rtti_policyEEEEEEEE8allocateEjPKv, ocp, 0x10) ||
+// 		isFunction(&_ZN9__gnu_cxx13new_allocatorIN5boost11multi_index6detail18ordered_index_nodeINS4_INS3_15index_node_baseIN13ClientManager11TimeoutInfoEEEEEEEE8allocateEjPKv, ocp, 0x20) ||
+// 		isFunction(&_ZN9__gnu_cxx13new_allocatorI8PppStateE8allocateEjPKv, ocp, 0x20) ||
+// 		isFunction(&_ZNSs12_S_constructIPKcEEPcT_S3_RKSaIcESt20forward_iterator_tag, ocp, 0x80) ||
+// 		isFunction(&_ZN13shared_membufC1Ej, ocp, 0x40) ||
+// 		isFunction(&_ZN10__gnu_norm10_List_baseISt4pairISs14TDateTimeEntryESaIS3_EE11_M_get_nodeEv, ocp, 0x40) ||
+// 		isFunction(&_ZN10__gnu_norm12_Vector_baseIN14TDateTimeEntry12TimeIntervalESaIS2_EE11_M_allocateEj, ocp, 0x40) ||
+// 		isFunction(&_ZNSs7reserveEj, ocp, 0x100) ||
+// 		isFunction(&_ZN5boost10statechart6detail8allocateI6OpenedSaIvEEEPvj, ocp, 0x100) ||
+// 		isFunction(&_ZNSsC1EPKcRKSaIcE, ocp, 0x40) ||
+// 		isFunction(&_ZN10__gnu_norm12_Vector_baseIN14TDateTimeEntry12TimeIntervalESaIS2_EEC2EjRKS3_, ocp, 0x80) ||
+// 		isFunction(&_ZN10__gnu_norm4listISt4pairISs14TDateTimeEntryESaIS3_EE14_M_create_nodeERKS3_, ocp, 0x80) ||
+// 		isFunction(&_ZN5boost10statechart12simple_stateI6Opened8PppStateNS_3mpl4listIN4mpl_2naES7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_EELNS0_12history_modeE0EEnwEj, ocp, 0x20) ||
+// 		isFunction(&_ZN5boost10statechart6detail8allocateI8PppStateSaIvEEEPvj, ocp, 0x100) ||
+// 		isFunction(&_ZN9__gnu_cxx13new_allocatorIN10__gnu_norm10_List_nodeIN5boost13intrusive_ptrINS3_10statechart6detail10leaf_stateISaIvENS6_11rtti_policyEEEEEEEE8allocateEjPKv, ocp, 0x20) ||
+// 		isFunction(&_ZN9__gnu_cxx13new_allocatorIN10__gnu_norm10_List_nodeI7TSubnetEEE8allocateEjPKv, ocp, 0x20) ||
+// 		isFunction(&_ZNSs6appendEPKcj, ocp, 0x100) ||
+// 		isFunction(&_ZN5boost6detail12shared_countC1I10iALG_R3411EEPT_, ocp, 0x100) ||
+// 		isFunction(&_ZN5boost6detail12shared_countC1IPcNS_21checked_array_deleterIcEEEET_T0_, ocp, 0x100) ||
+// 		isFunction(&_ZN10__gnu_norm4listISt4pairISs14TDateTimeEntryESaIS3_EE9_M_insertENS_14_List_iteratorIS3_EERKS3_, ocp, 0x40) ||
+// 		isFunction(&_ZN10__gnu_norm6vectorIN14TDateTimeEntry12TimeIntervalESaIS2_EEC2ERKS4_, ocp, 0x100) ||
+// 		isFunction(&_ZN10__gnu_norm10_List_baseIN5boost13intrusive_ptrINS1_10statechart6detail10leaf_stateISaIvENS4_11rtti_policyEEEEESaIS9_EE11_M_get_nodeEv, ocp, 0x40) ||
+// 		isFunction(&_ZN5boost10statechart12simple_stateI8PppState12StateMachine7InitialLNS0_12history_modeE0EEnwEj, ocp, 0x20) ||
+// 		isFunction(&_ZN5boost12shared_arrayIcEC1EPc, ocp, 0x40) ||
+// 		isFunction(&_ZN5boost10shared_ptrI10iALG_R3411EC1IS1_EEPT_, ocp, 0x80) ||
+// 		isFunction(&_ZN9__gnu_cxx13new_allocatorISt13_Rb_tree_nodeISt4pairIKSsN13ClientManager8PeerInfoEEEE8allocateEjPKv, ocp, 0x20) ||
+// 		isFunction(&_ZN10__gnu_norm4listISt4pairISs14TDateTimeEntryESaIS3_EE18_M_insert_dispatchINS_20_List_const_iteratorIS3_EEEEvNS_14_List_iteratorIS3_EET_SB_12__false_type, ocp, 0x80) ||
+// 		isFunction(&_ZN14TDateTimeEntryC1ERKS_, ocp, 0x40) ||
+// 		isFunction(&_ZN5boost10statechart12simple_stateI8PppState12StateMachine7InitialLNS0_12history_modeE0EE17shallow_constructERKPNS0_13state_machineIS3_S2_SaIvENS0_25null_exception_translatorEEERSA_, ocp, 0x100) ||
+// 		isFunction(&_ZN10__gnu_norm4listIN5boost13intrusive_ptrINS1_10statechart6detail10leaf_stateISaIvENS4_11rtti_policyEEEEESaIS9_EE14_M_create_nodeERKS9_, ocp, 0x80) ||
+// 		isFunction(&_ZN5boost10statechart12simple_stateI6Opened8PppStateNS_3mpl4listIN4mpl_2naES7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_S7_EELNS0_12history_modeE0EE17shallow_constructERKNS_13intrusive_ptrIS3_EERNS0_13state_machineI12StateMachineS3_SaIvENS0_25null_exception_translatorEEE, ocp, 0x100) ||
+// 		isFunction(&_ZN5boost11multi_index21multi_index_containerIN13ClientManager11TimeoutInfoENS0_10indexed_byINS0_18ordered_non_uniqueINS0_6memberIS3_7timevalXadL_ZNS3_2tvEEEEEN4mpl_2naESA_EENS0_14ordered_uniqueINS6_IS3_SsXadL_ZNS3_4hookEEEEESA_SA_EESA_SA_SA_SA_SA_SA_SA_SA_SA_SA_SA_SA_SA_SA_SA_SA_SA_SA_EESaIS3_EE13allocate_nodeEv, ocp, 0x40) ||
+// 		isFunction(&_ZN9__gnu_cxx13new_allocatorI7ClosingE8allocateEjPKv, ocp, 0x20) //||
+// 
+// 	)
+// 	{
+// 		int i = 0;
+// 		while (reinterpret_cast<uint32_t>(getReturnAddress(i)) != ocp) i++;
+// 		return getFilteredCallPoint (reinterpret_cast<uint32_t>(getReturnAddress(i + 1)));
+// 	}
 
 	return ocp;
 }
